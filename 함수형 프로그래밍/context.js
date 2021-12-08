b9613ab7067831738d8d5ef48e50a41668285eda
SeungWookHan <hswook12@me.com>
2021-12-08 20:03:44 +0900

    Add context

---
+++ 함수형 프로그래밍/context.js
@@ -0,0 +1,11 @@
+/*
+## 자바스크립트의 실행 컨텍스트
+
+- execution context를 통해 관리됨
+- 해당 실행 컨텍스트들이 콜스택에 쌓여서 순차적으로 실행됨
+- 초기에는 빌트인 객체인 Math, String, Array 등과 BOM, DOM
+- 이후부터
+  1. 스코프체인 생성과 초기화
+  2. Variable Object 형성
+  3. this value 결정
+*/
